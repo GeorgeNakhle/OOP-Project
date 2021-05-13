ace35e85d84eefcb398c526f95bb98343ea853a0
Antony Vasiliev <tony.vasiliev8@gmail.com>
2021-05-13 16:00:51 -0400

    cleaned up index.js, added css and jss get handlers, redid example mvc

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ const projectRoot = path.dirname(require.main.filename);
 process.env.models = path.resolve(projectRoot, 'mvc', 'models');
 process.env.views = path.resolve(projectRoot, 'mvc', 'views');
 process.env.controllers = path.resolve(projectRoot, 'mvc', 'controllers');
+// Save other shit in env
+process.env.styles = path.resolve(projectRoot, 'css');
+process.env.scripts = path.resolve(projectRoot, 'js');
 
 // Partial view renderer
 const {registerPartialTemplate} = require(`${process.env.controllers}/HandlebarsHelper`);
 
 // Port for the back-end to listen on
 const port = process.env.PORT || 8080;
+// Partials for views
+const PARTIALS = [ 'Header', 'Footer', 'Scripts', 'Styles' ];
 
 // Create a request listener express app
 const expressApp = express();
@@ -44,11 +49,22 @@ io.on('connection', socket => {
     })
 });
 
+const promises = [];
+for (const partial of PARTIALS){
+    const promise = registerPartialTemplate(partial, `${process.env.views}/partials/${partial}.hbs`);
+    promises.push(promise);
+}
 
-// Register Header and Footer partial views
-registerPartialTemplate('Header', `${process.env.views}/partials/Header.hbs`).then(() => {
-    registerPartialTemplate('Footer', `${process.env.views}/partials/Footer.hbs`).then(() => {
-        // Add routes here
-        expressApp.use('/example', require(`${process.env.controllers}/example.js`));
-    })
-})
+Promise.all(promises).then(() => {
+    // Get handlers for CSS Style
+    expressApp.get('/css/:filename', (req, res) => {
+        res.sendFile(`${process.env.styles}/${req.params.filename}`);
+    });
+    // Get handlers for JS Scripts
+    expressApp.get('/js/:filename', (req, res) => {
+        res.sendFile(`${process.env.scripts}/${req.params.filename}`);
+    });
+
+    // Add routes here
+    expressApp.use('/example', require(`${process.env.controllers}/example.js`));
+});
