c5f85d7e6263759bff4f00a6d2e30027f9b1f075
Antony Vasiliev <tony.vasiliev8@gmail.com>
2021-05-13 20:31:10 -0400

    fixed login front end stuff

--- js/login.js
+++ js/login.js
@@ -1,13 +1,32 @@
+if (getCookie('token')){
+    alert(`Already logged in as ${getCookie('username')}!`);
+    window.location = '/home';
+}
+
 function onSubmit(){
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    fetchAPI('login', {username, password}).then(token => {
-        setCookie(token);
-        window.location = '/home';
-    }).catch(err => {
-        deleteCookie();
-        console.error(err);
-        alert(`Login error: ${err.message}!`);
-    })
+    if (!username){
+        alert('Enter a username!')
+    }
+    else if (!password){
+        alert('Enter a password!');
+    }
+    else{
+        fetchAPI('login', {username, password}).then(res => {
+            setCookie('token', res.token);
+            setCookie('username', username);
+
+            window.location = '/home';
+        }).catch(err => {
+            clearCookies();
+            console.error(err);
+            alert(`Login error: ${err.message}!`);
+        })
+    }
+}
+
+function goRegister(){
+    window.location = '/register';
 }
