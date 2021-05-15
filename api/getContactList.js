70369825cdc2c97077c43a6f1d52a990e097f836
Antony Vasiliev <tony.vasiliev8@gmail.com>
2021-05-15 13:45:45 -0400

    added get contact list endpoint

--- api/getContactList.js
+++ api/getContactList.js
@@ -1,7 +1,14 @@
 const db = require(`${process.env.database}/db`);
 
 function doTheThing(request, response){
-    
+    const id = request.body.userID;
+
+    db.select(['added.id as "userID"', 'added.username as "username"'], ['user added', 'contact cont'], ['added.id = cont.user_added', `cont.added_by = ${id}`], true).then(contacts => {
+        response.status(200).end(JSON.stringify(contacts));
+    }).catch(err => {
+        console.error(err);
+        response.status(500).end(JSON.stringify({error: err.message}));
+    });
 }
 
 module.exports = doTheThing;
