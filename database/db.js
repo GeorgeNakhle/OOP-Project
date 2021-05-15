9e7cc9dfe09e9f13364c73c1b3b52b55794ab6ed
Antony Vasiliev <tony.vasiliev8@gmail.com>
2021-05-15 12:12:47 -0400

    added select insert and count handlers to database

--- database/db.js
+++ database/db.js
@@ -35,6 +35,7 @@ function _normalize(rowDataPacket){
 }
 
 function _normalizeMany(rowDataPackets){
+    console.log(rowDataPackets);
     const normed = [];
     for (const packet of rowDataPackets){
         normed.push(_normalize(packet));
@@ -42,17 +43,67 @@ function _normalizeMany(rowDataPackets){
     return normed;
 }
 
-function query(query){
+function _query(query){
     return new Promise((resolve, reject) => {
         connect().then(con => {
             con.query(query, (error, results) => {
                 con.end();
 
                 if (error) reject(error);
-                else resolve(_normalize(results));
+                else resolve(results);
             });
         })
     })
 }
 
-module.exports = {query};
+function count(from, where){
+    return new Promise((resolve, reject) => {
+        let query = `SELECT * FROM ${from.join(', ')} WHERE ${where.join(' AND ')}`;
+        _query(query).then(res => {
+            resolve(res.length);
+        }).catch(reject);
+    })
+}
+
+function select(what, from, where){
+    return new Promise((resolve, reject) => {
+        let query = `SELECT ${what.join(', ')} FROM ${from.join(', ')} WHERE ${where.join(' AND ')}`;
+        _query(query).then(res => {
+            const normed = _normalizeMany(res);
+            if (normed.length == 0){
+                reject(new Error('select query resulted empty'));
+            }
+            else{
+                resolve(normed);
+            }
+        }).catch(reject);
+    })
+}
+
+function insert(table, values){
+    return new Promise((resolve, reject) => {
+        const keys = [];
+        const vals = [];
+
+        for (const key in values){
+            const val = values[key];
+
+            keys.push('`' + key + '`');
+            
+            if (typeof val == typeof 'string'){
+                vals.push(`'${val}'`);
+            }
+            else{
+                vals.push(val);
+            }
+        }
+
+        const query = `INSERT INTO ${table}(${keys}) VALUES (${vals});`;
+        _query(query).then(res => {
+            const normed = _normalize(res);
+            resolve(normed);
+        }).catch(reject);
+    })
+}
+
+module.exports = {select, insert, count};
