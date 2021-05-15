e37e37e074baa3f1eb0043c1881d41bca696d3e3
Antony Vasiliev <tony.vasiliev8@gmail.com>
2021-05-15 14:35:03 -0400

    added create contact endpoint

--- api/createContact.js
+++ api/createContact.js
@@ -1,7 +1,40 @@
 const db = require(`${process.env.database}/db`);
 
-function doTheThing(request, response){
-    
+async function doTheThing(request, response){
+    const {currentUserID, contactID, contactUsername, nickname, notes} = request.body;
+
+    let user_added = null;
+
+    if (contactID){
+        user_added = contactID;
+    }
+    else{
+        try {
+            await db.select(['id'], ['user'], [`username = '${contactUsername}'`]).then(res => {
+                user_added = res[0].id;
+            })
+        } catch (error) {
+            console.error(error);
+            return response.status(500).end({error: error.message});
+        }
+    }
+
+    create(currentUserID, user_added, nickname, notes).then((resolved) => {
+        response.status(resolved.code).end('{}');
+    }).catch(rejected => {
+        response.status(rejected.code).end({error: rejected.content.message});
+    })
+}
+
+function create(added_by, user_added, nickname, notes){
+    return new Promise((resolve, reject) => {
+        db.insert('contact', {added_by, user_added, nickname, notes}).then(insert => {
+            resolve({code: 200});
+        }).catch(err => {
+            console.error(err);
+            reject({code: 500, content: new Error('Failed to create contact')});
+        });
+    })
 }
 
 module.exports = doTheThing;
