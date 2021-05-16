8a1cb862317caa62dfb130472615cd62cfe7b7fc
Benjamin Rouleau <benjamin.rouleau@me.com>
2021-05-16 01:45:56 -0400

    search chat and join chat implemented

--- api/searchForChat.js
+++ api/searchForChat.js
@@ -2,11 +2,38 @@ const db = require(`${process.env.database}/db`);
 const helper = require(`${process.env.api}/helper`);
 
 function http(request, response){
+    const {searchParams} = request.body;
 
+    model(searchParams).then(res => {
+        response.status(200).end(JSON.stringify(res));
+    }).catch(err => {
+        console.error(err);
+        response.status(500).end();
+    });
 }
 
-function model(){
-
+function model(searchString){
+    return new Promise((resolve, reject) => {
+        if (!searchString){
+            resolve({success: false, message: 'No search parameters provided!'});
+        }
+        else{
+            const what = [
+                'id', 
+                'chatname', 
+            ];
+            const from = [
+                'chat'
+            ];
+            const where = [
+                `chatname LIKE "%${searchString}%"`, 
+            ];
+    
+            db.select(what, from, where, true).then(chats => {
+                resolve({success: true, chats});
+            }).catch(reject);
+        }
+    })
 }
 
 module.exports = {http, model};
