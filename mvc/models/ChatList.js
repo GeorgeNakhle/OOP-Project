69c6db22cbcbeadced9a4184590496a096ea4c44
George Nakhle <1739127@johnabbottcollege.net>
2021-05-16 18:25:01 -0400

    Comments Complete

--- mvc/models/ChatList.js
+++ mvc/models/ChatList.js
@@ -1,13 +1,16 @@
 const getChatList = require(`${process.env.api}/getChatList`).model
 
 function doStuff(request) {
+    // Array to be passed into view
     let chats = [];
     return new Promise((resolve, reject) => {
 
+        // Current User ID from request response
         const currentUserID = request.query.currentUserID;
         if (currentUserID) {
             getChatList(currentUserID).then(res => {
                 if (res.success) {
+                    // Get query results from API
                     chats = res.chats;
 
                     resolve({
