82bc0363ad90abeb49c5f2ee40e8af66b27deaff
Antony Vasiliev <tony.vasiliev8@gmail.com>
2021-05-15 22:15:06 -0400

    removed history bc im dumb and it wasnt needed

--- js/main.js
+++ js/main.js
@@ -11,9 +11,6 @@ function correctURL(){
         if (!onLoginOrRegister){
             window.location = '/login';
         }
-        else{
-            saveCurrentPage();
-        }
     }
     else{
         if (onLoginOrRegister){
@@ -21,9 +18,6 @@ function correctURL(){
         }
         else if (!URLContainsID){
             window.location = `${path}?currentUserID=${currentUserID}`;
-        }
-        else{
-            saveCurrentPage();
         }
     }
 }
@@ -58,37 +52,6 @@ function fetchAPI(endpoint, data){
         });
     })
 }
-
-function saveCurrentPage(){
-    let history = getCookie('history');
-    if (!history) history = [];
-
-    const loc = window.location.pathname + window.location.search
-    if (history[history.length - 1] != loc){
-        history.push(loc);
-        setCookie('history', history);
-    }
-}
-
-function restorePreviousPage(){
-    const history = getCookie('history');
-    if (history){
-        if (history.length > 1){
-            const curr = history.pop();
-            const prev = history[history.length - 1];
-            setCookie('history', history);
-
-            if (prev){
-                window.location = prev;
-            }
-            else{
-                window.location = '/';
-            }
-        }
-    }
-}
-
-console.log(getCookie('history'));
 
 /**
  * Deletes local cookie
