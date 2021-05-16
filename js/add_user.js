e3fe0e0eabbd030d7987eb7952bf4e03ae80c799
Benjamin Rouleau <benjamin.rouleau@me.com>
2021-05-16 04:00:22 -0400

    dummy adduser

--- js/add_user.js
+++ js/add_user.js
@@ -2,7 +2,17 @@ function addUsername() {
     //add by username
     alert("add username");
 }
-function addContact() {
+function addContact(id) {
+    console.log(id);
+    let chat_id = 1;
+    fetchAPI('add-to-chat', {chat_id: chat_id, user_id: id }).then(res => {
+        if(res.success){
+            alert(`successfully added user to the chat!`);
+            window.location = `/chat?currentUserID=${getCookie('currentUserID')}&id=${chat_id}`
+        } else {
+            alert("failed to join chat");
+        }
+    });
     //add from contacts
-    alert("add contact");
+    //alert("add contact");
 }
